c0fd4566507a3481803ced615d11fc287898ffcf
Regi Fernando <najoanregi@gmail.com>
2026-01-19 15:43:07 +0800

    fist commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int umur = 20;
+        String nama = "Seon";
+        System.out.println("Umur saya adalah: " + umur + " tahun.");
+        System.out.println("Nama saya adalah: " + nama);
+    }
+}
