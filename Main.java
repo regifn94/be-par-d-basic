b86d4723478474d504505ad1ef58b81a920ff88e
Regi Fernando <najoanregi@gmail.com>
2026-01-21 15:39:30 +0800

    data type and list maps

--- Main.java
+++ Main.java
@@ -1,10 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
-        int umur = 20;
-        String nama = "Seon";
-        System.out.println("Umur saya adalah: " + umur + " tahun.");
-        System.out.println("Nama saya adalah: " + nama);
+        // int umur = 20;
+        // String nama = "Seon";
+        // System.out.println("Umur saya adalah: " + umur + " tahun.");
+        // System.out.println("Nama saya adalah: " + nama);
+
+        // // int number = null;
+        // Integer numbers = null;
+
+        // int a = 10;
+        // int b = 7;
+        // int c = a + b;
+        // System.out.println("Hasil penjumlahan " + a + " + " + b + " = " + c);
+
+        // c = a - b;
+        // System.out.println("Hasil pengurangan " + a + " - " + b + " = " + c);
+
+        // c = a * b;
+        // System.out.println("Hasil perkalian " + a + " * " + b + " = " + c);
+
+        // c = a / b;
+        // System.out.println("Hasil pembagian " + a + " / " + b + " = " + c);
+
+        // c = a % b;
+        // System.out.println("Hasil modulus " + a + " % " + b + " = " + c);
+
+        String[] arrName = new String[50];
+        List<String> namesBeD = new ArrayList<>();
+        namesBeD.add("Seon");
+        namesBeD.add("Tambengi");
+        namesBeD.add("Montela");
+
+        Map<String, List<String>> intStr = new HashMap<>();
+
+        intStr.put("BE-D", namesBeD);
+
+        System.out.println(intStr.get("BE-D"));
+
     }
 }
